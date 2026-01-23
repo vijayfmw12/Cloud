efaa2d4c9ae5d05835f4330eb771a6a2b3d3f5f1
vijayfmw <vijayfmw12@gmail.com>
2026-01-23 17:39:20 -0600

    settings

---
+++ Terraform/settings/settings.tf
@@ -0,0 +1,14 @@
+# TF Settings block
+
+terraform {
+# TF version
+	required_version = "~>1.14.3"
+
+#Provider Versions
+	required_providers {
+		google = {
+			source = "hashicorp/google"
+			version = "~>7.14.0"
+		}
+	}
+}
