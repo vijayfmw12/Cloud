d8cd91173061203932e92808c33f9f9b1c2d7a09
vijayfmw <vijayfmw12@gmail.com>
2026-01-22 22:22:48 -0600

    sampletestfile

---
+++ Terraform/First-provider/01-vpc.tf
@@ -0,0 +1,5 @@
+# create a custom vpc
+resource "google_compute_network" "vpc1" {
+name = "custom-vpc-tf"
+auto_create_subnetworks = false
+}
--- Terraform/First-provider/vpc.tf
+++
@@ -1,5 +0,0 @@
-# create a custom vpc
-resource "google_compute_network" "vpc1" {
-name = "custom-vpc-tf"
-auto_create_subnetworks = false
-}
