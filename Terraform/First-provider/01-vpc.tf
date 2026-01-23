ed9fb0d782d002f8df1e3c0a158281146b005c7e
vijayfmw <vijayfmw12@gmail.com>
2026-01-23 06:56:11 -0600

    create second vpc

--- Terraform/First-provider/01-vpc.tf
+++ Terraform/First-provider/01-vpc.tf
@@ -1,5 +1,13 @@
+# Resource-1
 # create a custom vpc
 resource "google_compute_network" "vpc1" {
 name = "custom-vpc-tf"
+auto_create_subnetworks = false
+}
+
+# Resource-2
+# create a custom vpc
+resource "google_compute_network" "vpc2" {
+name = "custom-vpc2-tf"
 auto_create_subnetworks = false
 }
