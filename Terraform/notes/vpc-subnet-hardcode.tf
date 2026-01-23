82a9714628e13d04376f3d134da5a4aba11762e9
vijayfmw <vijayfmw12@gmail.com>
2026-01-23 11:24:19 -0600

    SN files create

---
+++ Terraform/notes/vpc-subnet-hardcode.tf
@@ -0,0 +1,16 @@
+# Create a VPC
+resource "google_compute_network" "vpc1" {
+    name = "custom-vpc1"
+    description = "vpc created from TF"
+    auto_create_subnetworks = false
+}
+
+
+# Create a subnet
+# Requirement: subnet-name, region, cidr-range, network name
+resource "google_compute_subnetwork" "vpc1-subnet" {
+    name = "subnet-x"
+    region = "us-central1"
+    ip_cidr_range = "10.2.0.0/16"
+    network = "custom-vpc1"
+    }
