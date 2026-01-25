c2d64ad999318f3d7cc392fd86ef375382945df0
vijayfmw <vijayfmw12@gmail.com>
2026-01-24 22:14:17 -0600

    removed default values for tfvars

--- Terraform/variable-block/1/variables.tf
+++ Terraform/variable-block/1/variables.tf
@@ -6,15 +6,12 @@ type = string
 variable "vm_zone" {
 description = "The zone in which vm should be created"
 type = string
-default = "us-central1-a"
 }
 
 variable "project_id" {
   type = string
-  default = "prod-gcp-2026"
 }
 
 variable "region" {
  type = string
- default = "us-central1"
 }
