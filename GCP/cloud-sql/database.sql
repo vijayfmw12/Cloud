1400faaf4cfb3a0d4767a76258de7dbd9e6060c2
vijayfmw12 <vijayfmw12@gmail.com>
2026-01-27 17:57:08 -0600

    syntax correction

--- GCP/cloud-sql/database.sql
+++ GCP/cloud-sql/database.sql
@@ -1,10 +1,10 @@
 create table users (
-    id int auto_increment primary key;
-    username varchar(50) not null unique;
+    id int auto_increment primary key,
+    username varchar(50) not null unique,
     password varchar(255) not null,
     role enum('admin','user') not null
 );
 
 insert into users (username, password, role) values
-('admin','adminpasswordhash','admin',),
+('admin','adminpasswordhash','admin'),
 ('user1','user1passwordhash','user');
