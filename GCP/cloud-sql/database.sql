113513fbd868cad6394c0d9f6b4df717861d05a3
vijayfmw12 <vijayfmw12@gmail.com>
2026-02-06 21:22:47 -0600

    db

--- GCP/cloud-sql/database.sql
+++ GCP/cloud-sql/database.sql
@@ -1,3 +1,10 @@
+
+create database weblogic;
+
+show databases;
+
+use weblogic;
+
 create table users (
     id int auto_increment primary key,
     username varchar(50) not null unique,
@@ -8,3 +15,29 @@ create table users (
 insert into users (username, password, role) values
 ('admin','adminpasswordhash','admin'),
 ('user1','user1passwordhash','user');
+
+select * from users;
+insert into users (username, password, role) values
+('user2','user2passwordhash','user2'),
+('user3','user3passwordhash','user3');
+
+insert into users (username, password, role) values
+('vijay','vijaypasswordhash','user');
+
+CREATE TABLE employee (
+    emp_id INT AUTO_INCREMENT PRIMARY KEY,
+    emp_name VARCHAR(100) NOT NULL,
+    department VARCHAR(50),
+    designation VARCHAR(50),
+    salary DECIMAL(10,2),
+    email VARCHAR(150) UNIQUE,
+    phone VARCHAR(15),
+    hire_date DATE,
+    status VARCHAR(20) DEFAULT 'ACTIVE',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+INSERT INTO employee (emp_id, emp_name, department, salary)
+VALUES (101, 'Vijay', 'IT', 125000);
