c4613bdca573bb19e7bfddf78abfa7e2ac8b75ea
vijayfmw12 <vijayfmw12@gmail.com>
2026-02-11 22:48:50 -0600

    thwa delete

--- tripuraresidents250625/tripuraresidents/residentsportal/src/main/resources/db_flywayBKP/migration/V2__add_enabled_and_password_columns.sql
+++
@@ -1,9 +0,0 @@
--- Add 'enabled' column with default value false to avoid null constraint violation
-ALTER TABLE users ADD COLUMN IF NOT EXISTS enabled BOOLEAN DEFAULT FALSE;
-
--- Add 'password' column with a temporary default to avoid null constraint violation
-ALTER TABLE users ADD COLUMN IF NOT EXISTS password VARCHAR(255) DEFAULT 'changeme123';
-
--- Optionally, if you want to enforce NOT NULL afterwards (once all values are valid), run this in a later version:
--- ALTER TABLE users ALTER COLUMN enabled SET NOT NULL;
--- ALTER TABLE users ALTER COLUMN password SET NOT NULL;
